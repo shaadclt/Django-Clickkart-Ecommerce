482c7c3c506a5faa5f97b63301bfd7975463eea9
Shaad <shaadclt@gmail.com>
2022-08-22 06:01:28 +0530

    reviews

--- clickkart/views.py
+++ clickkart/views.py
@@ -4,10 +4,10 @@ from store.models import Product,ReviewRating
 def home(request):
     products = Product.objects.all().filter(is_popular=True).order_by('created_date')
     for product in products:
-        reviews = ReviewRating.objects.filter(product_id = product.id,status=True)
+        
 
     context = {
         'products':products,
-        'reviews':reviews,
+
     }
     return render(request,'home.html',context)
