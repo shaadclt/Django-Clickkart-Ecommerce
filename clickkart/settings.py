50ea5122cc1a42b769fcf5a64fa69847d9a237d8
Shaad <shaadclt@gmail.com>
2022-08-22 05:58:09 +0530

    debug

--- clickkart/settings.py
+++ clickkart/settings.py
@@ -27,9 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['clickkart.herokuapp.com']
+
 
 
 # Application definition
