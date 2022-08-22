8743c6ebd596705840c93b839229f2d18c48ccea
Shaad <shaadclt@gmail.com>
2022-08-22 21:46:45 +0530

    s3

--- clickkart/settings.py
+++ clickkart/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['clickkart.herokuapp.com']
+ALLOWED_HOSTS = ['clickkart.herokuapp.com',"*"]
 
 
 
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'adminapp',
     'coupon',
+    'storages',
     ]
 
 MIDDLEWARE = [
@@ -135,12 +136,31 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR /'static'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR /'static'
+# STATICFILES_DIRS = [
+#     'clickkart/static',
+# ]
+django_heroku.settings(locals())
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
+
 STATICFILES_DIRS = [
     'clickkart/static',
 ]
-django_heroku.settings(locals())
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'clickkart.media_storages.MediaStorage'
 
 # media files configuration 
 MEDIA_URL = '/media/'
